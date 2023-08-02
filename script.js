380133bd323da9b0970072c840e3cdb82ce13281
PedroVidal2703 <pedrovidal2703@gmail.com>
2023-08-02 15:17:40 -0300

    feat: add carousel

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+var selectedSlide;
+
 window.onload = function () {
+	selectedSlide = "slide-1";
+	changeSlide(selectedSlide);
 	quoteObj = getQuote();
 	document.getElementById("quote").innerHTML = `"${quoteObj.quote}"`;
 	document.getElementById("author").innerHTML = `- ${quoteObj.author}`;
@@ -6,6 +10,17 @@ window.onload = function () {
 
 function getQuote() {
 	return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+function changeSlide(id) {
+	document.getElementById(selectedSlide).classList.add("animation-color");
+	document.getElementById(selectedSlide).classList.remove("selected-slide");
+	document.getElementById(id).classList.add("selected-slide");
+	document.getElementById(id).classList.remove("animation-color");
+	selectedSlide = id;
+
+	document.getElementById("aboutme-img").src = `${slides[selectedSlide].image}`;
+	document.getElementById("aboutme-text").innerHTML = `${slides[selectedSlide].text}`;
 }
 
 var quotes = [
@@ -34,3 +49,22 @@ var quotes = [
 		quote: "Tudo que ouvimos é uma opinião, não um fato. Tudo que vemos é uma perspectiva, não a verdade."
 	}
 ];
+
+var slides = {
+	"slide-1": {
+		image: "assets/caraguatatuba.JPG",
+		text: "Esse é um teste do slide, bla bla bla"
+	},
+	"slide-2": {
+		image: "assets/placeholder2.jpeg",
+		text: "Esse já é o segundo slide, tá funcionando até bem né?"
+	},
+	"slide-3": {
+		image: "",
+		text: "Esse vai ser o terceiro slide."
+	},
+	"slide-4": {
+		image: "",
+		text: "Quarto slide, testando, deu certo?"
+	}
+};
